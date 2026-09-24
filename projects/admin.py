bb10dd6421698ddf651c3777bbccad80ce48bc93
Rehan Khan <rehankhan.rk.0156@gmail.com>
2026-09-24 20:15:05 +0530

    Clear main branch for fresh project setup

--- projects/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-
-from django.contrib import admin
-from .models import Project, ProjectMember
-
-admin.site.register(Project)
-admin.site.register(ProjectMember)
